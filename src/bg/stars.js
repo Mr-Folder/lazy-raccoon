e095399036f1473dd133175f393de012a38d98cf
mr-folder <melagomen@gmail.com>
2024-12-01 21:32:19 +0100

    add everything else

---
+++ src/bg/stars.js
@@ -0,0 +1,28 @@
+var limit = 1001;
+space = document.getElementById("space");
+
+stars = {
+  rand: function() {
+    return Math.random();
+  },
+
+  createStar: function() {
+    let star = document.createElement("div");
+    star.className = "star";
+
+    return star;
+  },
+
+  create: function() {
+    console.log("create star");
+    for (let i = 0; i <= limit; i++) {
+      let star = this.createStar();
+      star.style.top = `${this.rand() * 100}%`;
+      star.style.left = `${this.rand() * 100}%`;
+      star.style.animationDelay = `${this.rand() * 8}s`;
+      space.appendChild(star);
+    }
+  }
+};
+
+stars.create();
