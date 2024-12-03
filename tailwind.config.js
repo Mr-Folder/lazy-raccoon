51dc5c89be78f7a07e17146b68b99cbff46a0873
mr-folder <melagomen@gmail.com>
2024-12-03 18:59:24 +0100

    add some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        'dark': '#121212',
+        // Base Colors
+        'dark': '#121212', // Pure black or dark background
         'light-grey': '#E0E0E0',
-        'light-white': '#FAFAFA'
+        'light-white': '#FAFAFA',
+        // Pure black
+        "black": "#000000",
+        "dark-1": "#0A0A0A",
+        "dark-2": "#0E0E0E",
+        "dark-3": "#161616",
+        "dark-4": "#2B2B2B",
+        // Neon Colors
       },
       fontFamily: {
         feijoa: 'feijoa',
-      }
+      },
     },
   },
   plugins: [],
