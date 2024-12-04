9a148794524510d34d242f58198b1c8bfe1a1920
mr-folder <melagomen@gmail.com>
2024-12-04 15:39:18 +0100

    fix base repo name for vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 export default defineConfig({
   plugins: [react()],
+  base: "/lazy-raccoon/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
